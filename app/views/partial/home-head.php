52b84166422ad3ea2cb7a404822eb41a8e143df2
Ray <mofies@gmail.com>
2014-08-04 20:50:37 +0800

    fix css and add OG meta

--- app/views/partial/home-head.php
+++ app/views/partial/home-head.php
@@ -2,6 +2,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <meta name="description" content="<?= Site::first()->description ?>">
+
+  <meta property="og:type" content="website"/>
+  <meta property="og:site_name" content="LC Stone"/>
+  <meta property="og:title" content="<?= Site::first()->title ?>"/>
+  <meta property="og:description" content="<?= Site::first()->description ?>"/>
+  <meta property="og:url" content="http://nowyoufindme.no-ip.org:8888"/>
+  <meta property="og:image" content="http://nowyoufindme.no-ip.org:8888/files/assets/8/original/l_stone08.jpg"/>
+  <meta property="og:image" content="http://nowyoufindme.no-ip.org:8888/files/assets/3/large/l_stone03.jpg"/>
+
   <title><?= Site::first()->title ?></title>
   <?= css_tag("//cdn.jsdelivr.net/fontawesome/4.1.0/css/font-awesome.min.css") ?>
   <?= css_tag("frontsite/global.scss") ?>
