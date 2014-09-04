d4ff40d3b8ed37913e41f7444c06f7b4c4c8c5fc
nyfm <nyfm@nyfm.(none)>
2014-09-04 15:45:05 +0800

    product list added

--- app/controllers/contact_controller.php
+++ app/controllers/contact_controller.php
@@ -10,10 +10,23 @@
       $this->email = $this->params["email"];
       $this->company = $this->params["company"];
       $this->guest = $this->params["guest"];
+      $productlist = $this->params["product"];
+      $products = array();
+      foreach ($productlist as $product) {
+        $product = Products::find($product["id"]);
+        if($product){
+          array_push($products, array(
+              "id" => $product->id,
+              "title"  => $product->title,
+              "image" => ($product->assets_relation_ids[0] ? Assets::find($product->assets_relation_ids[0])->file["thumb"]->to_absolute_url() : "")
+          ));
+        }
+      }
+      $this->products = $products;
       $mailer = new Mailer();
       $mailer->setMsgFromMailer("contact_us_mailer");
       $mailer->AddAddress(MAIL_USERNAME,MAIL_NAME);
-      $mailer->Subject = "A new contact us mail.";
+      $mailer->Subject = (count($products) > 0 ? "產品詢問" : "訪客來信");
       $mailer->Send();
       return renderJson(array("success" => true));
     }
--- app/mailers/contact_us_mailer.php
+++ app/mailers/contact_us_mailer.php
@@ -7,16 +7,16 @@
     <table id="ask" width="800" border="0" cellpadding="0" cellspacing="0" style="margin: 0 auto">
       <tr>
         <td colspan="3">
-          <img src="http://nowyoufindme.no-ip.org:8888/public/email/edm_01.png" width="800" height="192" alt="" style="display: block;">
+          <img src="<?= Request::server_name_with_port() ?>/public/email/edm_01.png" width="800" height="192" alt="" style="display: block;">
         </td>
       </tr>
       <tr>
         <td style="width: 104px;">
-          <img src="http://nowyoufindme.no-ip.org:8888/public/email/edm_02.png" width="104" height="136" alt="" style="display: block;">
+          <img src="<?= Request::server_name_with_port() ?>/public/email/edm_02.png" width="104" height="136" alt="" style="display: block;">
         </td>
-        <td style="width: 588px; height: 136px; background: url(http://nowyoufindme.no-ip.org:8888/public/email/edm_03.png) center no-repeat transparent; vertical-align: top;">
+        <td style="width: 588px; height: 136px; background: url(<?= Request::server_name_with_port() ?>/public/email/edm_03.png) center no-repeat transparent; vertical-align: top;">
           <h1 style="line-height: 68px; margin: 1.5em 0 0; text-align: center; font-family: '微軟正黑體'; color: #5F5A5D;">
-            <?php if ($productList) {
+            <?php if ($products) {
               echo '產品詢問';
             } else {
               echo "訪客來信";
@@ -24,11 +24,11 @@
           </h1>
         </td>
         <td style="width: 108px;">
-          <img src="http://nowyoufindme.no-ip.org:8888/public/email/edm_04.png" width="108" height="136" alt="" style="display: block;">
+          <img src="<?= Request::server_name_with_port() ?>/public/email/edm_04.png" width="108" height="136" alt="" style="display: block;">
         </td>
       </tr>
       <tr>
-        <td colspan="3" style="background: url(http://nowyoufindme.no-ip.org:8888/public/email/edm_05.png) center repeat-y transparent;">
+        <td colspan="3" style="background: url(<?= Request::server_name_with_port() ?>/public/email/edm_05.png) center repeat-y transparent;">
           <div style="width: 550px; margin: 0 auto; color: #585858;">
             <table style="color: #585858;">
               <tbody style="font-size: 14px; line-height: 2em;">
@@ -54,28 +54,18 @@
                 </tr>
               </tbody>
             </table>
-            <?php if ($productList) { ?>
+            <?php if ($products) { ?>
               <hr style="border-style: dashed; border-color: #D3D3D3; border-width: 1px 0 0; margin-bottom: 2em;">
               <h2 style="font-size: 16px; text-align: center;">產品詢問清單</h2>
               <ol style="list-style: none; padding: 0; width: 100%; margin: 0 auto;">
+              <?php foreach ($products as $product) { ?>
                 <li style="padding: 5px; font-size: 14px; line-height: 30px; float: left; width: 45%; margin-bottom: 1em;">
-                  <div style="float: left; width: 30px; height: 30px; border-radius: 50%; overflow: hidden;">
-                    <img src="http://nowyoufindme.no-ip.org:8888/files/assets/3/thumb/l_stone03.jpg" alt="" style="width: 100%; height: 100%;">
+                  <div style="float: left; width: 30px; height: 30px; overflow: hidden;">
+                    <img src="<?= Request::server_name_with_port() . $product['image'] ?>" alt="" style="width: 100%; height: auto;">
                   </div>
-                  <a href="/collections/12" target="_blink" style="display: inline-block; margin-left: 10px; text-decoration: blink; color: #42AED8;">黃木紋亂片</a>
+                  <a href="<?= Request::server_name_with_port() . end(Routes::url_helper('collections','show',array('id' => $product['id']))) ?>" target="_blank" style="display: inline-block; margin-left: 10px; text-decoration: blink; color: #42AED8;"><?= $product['title'] ?></a>
                 </li>
-                <li style="padding: 5px; font-size: 14px; line-height: 30px; float: left; width: 45%; margin-bottom: 1em;">
-                  <div style="float: left; width: 30px; height: 30px; border-radius: 50%; overflow: hidden;">
-                    <img src="http://nowyoufindme.no-ip.org:8888/files/assets/3/thumb/l_stone03.jpg" alt="" style="width: 100%; height: 100%;">
-                  </div>
-                  <a href="/collections/12" target="_blink" style="display: inline-block; margin-left: 10px; text-decoration: blink; color: #42AED8;">黃木紋亂片</a>
-                </li>
-                <li style="padding: 5px; font-size: 14px; line-height: 30px; float: left; width: 45%; margin-bottom: 1em;">
-                  <div style="float: left; width: 30px; height: 30px; border-radius: 50%; overflow: hidden;">
-                    <img src="http://nowyoufindme.no-ip.org:8888/files/assets/3/thumb/l_stone03.jpg" alt="" style="width: 100%; height: 100%;">
-                  </div>
-                  <a href="/collections/12" target="_blink" style="display: inline-block; margin-left: 10px; text-decoration: blink; color: #42AED8;">黃木紋亂片</a>
-                </li>
+                <?php } ?>
               </ol>
             <?php } ?>
           </div>
@@ -83,7 +73,7 @@
       </tr>
       <tr>
         <td colspan="3">
-          <img src="http://nowyoufindme.no-ip.org:8888/public/email/edm_06.png" width="800" height="70" alt="" style="display: block;">
+          <img src="<?= Request::server_name_with_port() ?>/public/email/edm_06.png" width="800" height="70" alt="" style="display: block;">
         </td>
       </tr>
     </table>
